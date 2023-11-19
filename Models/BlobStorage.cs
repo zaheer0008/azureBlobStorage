cc27398fc43fc1dbf8b3bda1b4ae1a090df14227
Zaheer Ahmed <zaheer0008@gmail.com>
2023-11-19 15:26:34 +0500

    first commit

---
+++ Models/BlobStorage.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Azure;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace FileUploader.Models
+{
+    public class BlobStorage : IStorage
+    {
+        private readonly AzureStorageConfig storageConfig;
+
+        public BlobStorage(IOptions<AzureStorageConfig> storageConfig)
+        {
+            this.storageConfig = storageConfig.Value;
+        }
+
+        public Task Initialize()
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(storageConfig.ConnectionString);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(storageConfig.FileContainerName);
+            return containerClient.CreateIfNotExistsAsync();
+        }
+
+        public Task Save(Stream fileStream, string name)
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(storageConfig.ConnectionString);
+
+            // Get the container (folder) the file will be saved in
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(storageConfig.FileContainerName);
+
+            // Get the Blob Client used to interact with (including create) the blob
+            BlobClient blobClient = containerClient.GetBlobClient(name);
+
+            // Upload the blob
+            return blobClient.UploadAsync(fileStream);
+        }
+
+        public async Task<IEnumerable<string>> GetNames()
+        {
+             List<string> names = new List<string>();
+
+            BlobServiceClient blobServiceClient = new BlobServiceClient(storageConfig.ConnectionString);
+
+            // Get the container the blobs are saved in
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(storageConfig.FileContainerName);
+
+            // This gets the info about the blobs in the container
+            AsyncPageable<BlobItem> blobs = containerClient.GetBlobsAsync();
+
+            await foreach (var blob in blobs)
+            {
+                names.Add(blob.Name);
+            }
+            return names;
+        }
+
+        public Task<Stream> Load(string name)
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(storageConfig.ConnectionString);
+
+            // Get the container the blobs are saved in
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(storageConfig.FileContainerName);
+
+            // Get a client to operate on the blob so we can read it.
+            BlobClient blobClient = containerClient.GetBlobClient(name);
+
+            return blobClient.OpenReadAsync();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using FileUploader.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Hosting;
+
+namespace FileUploader
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Set up IOptions and populate AzureStorageConfig from configuration
+            services.AddOptions();
+            services.Configure<AzureStorageConfig>(Configuration.GetSection("AzureStorageConfig"));
+
+            // Wire up a single instance of BlobStorage, calling Initialize() when we first use it.
+            services.AddSingleton<IStorage>(serviceProvider => {
+                var blobStorage = new BlobStorage(serviceProvider.GetService<IOptions<AzureStorageConfig>>());
+                blobStorage.Initialize().GetAwaiter().GetResult();
+                return blobStorage;
+            });
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(routes =>
+            {
+                routes.MapDefaultControllerRoute();
+                routes.MapControllers();
+            });
+        }
+    }
+}
